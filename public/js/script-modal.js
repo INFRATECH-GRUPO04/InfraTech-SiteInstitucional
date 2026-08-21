a31c4de9cfd6626cb9eff723be0cec4bea1c1d5e
GuiPastorello <guilherme.pastorello@sptech.school>
2026-08-20 21:21:54 -0300

    Integrando o Modal de login e cadastro a tela inicial

---
+++ public/js/script-modal.js
@@ -0,0 +1,34 @@
+const openModalButtons = document.querySelectorAll('.open-modal');
+
+
+openModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const modalId = button.getAttribute('data-modal');
+        const modal = document.getElementById(modalId);
+
+        modal.showModal();
+    });
+});
+
+const switchModalButtons = document.querySelectorAll('.switch-modal');
+switchModalButtons.forEach(button => {
+    button.addEventListener('click', event => {
+        event.preventDefault();
+
+        const currentModal = button.closest('dialog');
+        const modalId = button.getAttribute('data-modal');
+        const targetModal = document.getElementById(modalId);
+
+        currentModal.close();
+        targetModal.showModal();
+    });
+});
+
+const closeModalButtons = document.querySelectorAll('.close-modal');
+closeModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const modal = button.closest('dialog');
+
+        modal.close();
+    });
+});
