b0a9e020e8f4c522d46dbc80da06087e7e04e28e
MariaEduardaFontanaNogueiro <maria.nogueiro@sptech.school>
2026-09-08 13:19:41 -0300

    fix: corrigir erro de token

--- src/models/login_cadastroModel.js
+++ src/models/login_cadastroModel.js
@@ -19,11 +19,11 @@ function autenticar(email, senha) {
 }
 
 
-function cadastrar(nome, email, senha, cpf, fkEmpresa) {
-    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, email, senha, cpf, fkEmpresa);
+function cadastrar(nome, email, senha, cpf, fkEmpresa, tipoAcesso) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, email, senha, cpf, fkEmpresa, tipoAcesso);
     
     var instrucaoSql = `
-        INSERT INTO funcionario (nome, email, senha, cpf, fkEmpresa) VALUES ('${nome}', '${email}', '${senha}', '${cpf}', ${fkEmpresa});`;
+        INSERT INTO funcionario (nome, email, senha, cpf, fkEmpresa, tipoAcesso) VALUES ('${nome}', '${email}', '${senha}', '${cpf}', ${fkEmpresa}, ${tipoAcesso});`;
     console.log("Executando a instrução SQL de cadastro: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
 }
--- src/routes/crypto.js
+++ src/routes/crypto.js
@@ -2,10 +2,17 @@ var crypto = require("crypto");
 var express = require("express");
 var router = express.Router();
 
+
 var cryptoController = require("../controllers/cryptoController");
 
 router.post("/gerar", function (req, res) {
     cryptoController.gerarCodigo(req, res);
 });
+
+router.get("/buscar/:codigo", function (req, res){
+    cryptoController.buscarCodigo(req, res);
+})
+
+router.put("/aualizar/:idConvite", function (req, res))
 
 module.exports = router;
