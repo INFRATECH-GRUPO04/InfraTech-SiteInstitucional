5b7ddaf68bd6d0d1c2503191ebaded564c5eb2ae
GuiPastorello <guilherme.pastorello@sptech.school>
2026-09-10 17:08:58 -0300

    fix: arruma cor do option do cadastrar servidor, deleta coisas desnecessarias do web-data-viz, remove a localizacao do servidor

--- src/controllers/servidorController.js
+++ src/controllers/servidorController.js
@@ -4,17 +4,15 @@ var servidorModel = require("../models/servidorModel");
 function cadastrar(req, res) {
     // Crie uma variável que vá recuperar os valores do arquivo cadastro.html
     var nomeServidor = req.body.nomeServer;
-    var localizacaoServidor = req.body.localizacaoServer;
+ 
 
     // Faça as validações dos valores
     if (nomeServidor == undefined) {
         res.status(400).send("Seu nome está undefined!");
-    } else if (localizacaoServidor == undefined) {
-        res.status(400).send("Seu email está undefined!");
     } else {
 
         // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
-        servidorModel.cadastrar(nomeServidor, localizacaoServidor)
+        servidorModel.cadastrar(nomeServidor)
             .then(
                 function (resultado) {
                     var idServidor = resultado.insertId;
--- src/models/servidorModel.js
+++ src/models/servidorModel.js
@@ -1,7 +1,7 @@
 var database = require("../database/config")
 
 function cadastrar(nome) {
-    console.log("ACESSEI O SERVIDOR MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, localizacao);
+    console.log("ACESSEI O SERVIDOR MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome);
     
     // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
     //  e na ordem de inserção dos dados.
@@ -13,8 +13,11 @@ function cadastrar(nome) {
 }
 
     function cadastrarComponente(fkServidor, fkComponente, capacidade,limiteAlerta) {
+            console.log("ACESSEI O SERVIDOR MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():");
+
     const instrucaoSql = `
         INSERT INTO servidor_has_componente (fkServidor, fkComponente, capacidade, limiteAlerta) VALUES(${fkServidor}, ${fkComponente}, ${capacidade}, ${limiteAlerta});`;
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
 
     return database.executar(instrucaoSql);
 }
