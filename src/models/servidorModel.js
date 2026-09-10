276b84ae7b250fbbe2ce026482db4d55208fb1b7
Jefferson-Palma <jefferson.palma@sptech.school>
2026-09-10 16:34:22 -0300

    fix:ajuste models e banco

--- src/models/servidorModel.js
+++ src/models/servidorModel.js
@@ -1,12 +1,12 @@
 var database = require("../database/config")
 
-function cadastrar(nome, localizacao) {
+function cadastrar(nome) {
     console.log("ACESSEI O SERVIDOR MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, localizacao);
     
     // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
     //  e na ordem de inserção dos dados.
     var instrucaoSql = `
-        INSERT INTO servidor (nome, localizacao) VALUES ('${nome}', '${localizacao}');
+        INSERT INTO servidor (nome) VALUE ('${nome}');
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
